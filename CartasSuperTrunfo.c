c8e9a6c6b6e964501e859d5da5dd5df09864b4b7
JoaquimTeixeira81 <joaquimnonato20@gmail.com>
2025-07-25 12:55:03 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,12 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Joaquim Teixeira
 
 int main() {
+    // Atributo da carta A01: São Paulo
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
